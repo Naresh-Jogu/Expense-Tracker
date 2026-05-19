8f69c2140e27bfd2bd6b94a2a299b81a3848a561
Naresh Jogu <kumarn29346@gmail.com>
2026-05-19 13:15:49 +0530

    adjusted styles

--- frontend/expese-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expese-tracker/src/pages/Auth/SignUp.jsx
@@ -16,9 +16,9 @@ const SignUp = () => {
 
   return (
     <AuthLayout>
-      <div className="lg:w-[100%] h-auto md:h-full md:mt-0 flex flex-col justify-center">
+      <div className="lg:w-full h-auto md:h-full md:mt-0 flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Create an Account</h3>
-        <p className="text-xs text-slate-700 mt-[5px] mb-6 ">
+        <p className="text-xs text-slate-700 mt-1 mb-6 ">
           Join us today by entering your details below.
         </p>
         <form onSubmit={handleSignup}>
